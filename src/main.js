f00faf37bce24fd94e8152cc5370bd893bb7920b
sunbeyond <sunbeyond@gmail.com>
2021-01-22 19:25:57 +0800

    coding

--- src/main.js
+++ src/main.js
@@ -17,25 +17,6 @@ import store from './store'
 
 const app = createApp(App)
 
-const requireComponent = require.context(
-  './components/tokens',
-  false, // 是否查询其子目录
-  /[A-Z]\w+\.(vue|js)$/
-)
-
-requireComponent.keys().forEach(fileName => {
-  const config = requireComponent(fileName)
-  const body = config.default || config
-  const name = body.name
-    || kebabCase(fileName
-        .split('/')
-        .pop()
-        .replace(/\.\w+$/, '')
-      )
-
-  app.component(name, body)
-})
-
 app.mixin({
     computed: {
       $store () {
